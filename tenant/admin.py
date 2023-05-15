0a6caa1ebefada26cd9611c63f14ad8db62066f1
Yao Josué <46876900+MJoshua25@users.noreply.github.com>
2023-05-15 21:35:08 +0000

    makemigrations Migrate admin_generator

--- tenant/admin.py
+++ tenant/admin.py
@@ -1,3 +1,32 @@
+# vim: set fileencoding=utf-8 :
 from django.contrib import admin
 
-# Register your models here.
+import tenant.models as models
+
+
+class TenantAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'statut',
+        'date_add',
+        'date_upd',
+        'name',
+        'unique_domain',
+    )
+    list_filter = (
+        'statut',
+        'date_add',
+        'date_upd',
+        'id',
+        'name',
+        'unique_domain',
+    )
+    search_fields = ('name',)
+
+
+def _register(model, admin_class):
+    admin.site.register(model, admin_class)
+
+
+_register(models.Tenant, TenantAdmin)
