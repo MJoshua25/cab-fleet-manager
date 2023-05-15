6ca18406972c3052c7f23cb74786c8ed1e2fa727
Yao Josué <46876900+MJoshua25@users.noreply.github.com>
2023-05-15 21:29:51 +0000

    creation des models de tenant

--- tenant/models.py
+++ tenant/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from core.models import StandardModel
+from django.utils.text import slugify
+
 
 # Create your models here.
+
+class Tenant(StandardModel):
+	name = models.CharField(max_length=100)
+	unique_domain = models.CharField(unique=True, max_length=255)
+
+	class Meta:
+		verbose_name = "Tenant"
+		verbose_name_plural = "Tenants"
+
+	def __str__(self) -> 'str':
+		return f'{self.unique_domain}'
+
+
+class TenantAwareModel(StandardModel):
+	tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+
+	class Meta:
+		abstract = True
