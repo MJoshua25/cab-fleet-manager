373d327cf7f3a222649bc36d484e023d3b8e5f8c
Yao Josué <46876900+MJoshua25@users.noreply.github.com>
2023-05-22 12:29:51 +0000

    create FleetUser model

--- fleet_app/models.py
+++ fleet_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from tenant.models import TenantAwareModel
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class FleetUser(TenantAwareModel):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+
+	class Meta:
+		verbose_name = "FleetUser"
+		verbose_name_plural = "FleetUsers"
+
+	def __str__(self):
+		return self.user.username
