a4336f2a3914222899a7f62b32ac4ae12b6be597
Yao Josué <46876900+MJoshua25@users.noreply.github.com>
2023-05-22 15:16:31 +0000

    makemigrations migrate Admin_generator

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
+# vim: set fileencoding=utf-8 :
 from django.contrib import admin
 
-# Register your models here.
+import core.models as models
+
+
+class DayOfTheWeekAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'day')
+    list_filter = ('id', 'day')
+
+
+def _register(model, admin_class):
+    admin.site.register(model, admin_class)
+
+
+_register(models.DayOfTheWeek, DayOfTheWeekAdmin)
--- fleet_app/admin.py
+++ fleet_app/admin.py
@@ -1,3 +1,103 @@
+# vim: set fileencoding=utf-8 :
 from django.contrib import admin
 
-# Register your models here.
+import fleet_app.models as models
+
+
+class FleetUserAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'statut', 'date_add', 'date_upd', 'tenant', 'user')
+    list_filter = ('statut', 'date_add', 'date_upd', 'tenant', 'user', 'id')
+
+
+class CarAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'statut',
+        'date_add',
+        'date_upd',
+        'tenant',
+        'model',
+        'brand',
+        'matriculation',
+        'color',
+        'on_service',
+    )
+    list_filter = (
+        'statut',
+        'date_add',
+        'date_upd',
+        'tenant',
+        'on_service',
+        'id',
+        'model',
+        'brand',
+        'matriculation',
+        'color',
+    )
+
+
+class DriverAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'statut',
+        'date_add',
+        'date_upd',
+        'tenant',
+        'last_name',
+        'first_name',
+        'phone_number',
+        'license_number',
+    )
+    list_filter = (
+        'statut',
+        'date_add',
+        'date_upd',
+        'tenant',
+        'id',
+        'last_name',
+        'first_name',
+        'phone_number',
+        'license_number',
+    )
+
+
+class ContractAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'statut',
+        'date_add',
+        'date_upd',
+        'tenant',
+        'driver',
+        'car',
+        'is_active',
+        'expect_daily_revenue',
+        'holiday_expect_revenu',
+    )
+    list_filter = (
+        'statut',
+        'date_add',
+        'date_upd',
+        'tenant',
+        'driver',
+        'car',
+        'is_active',
+        'id',
+        'expect_daily_revenue',
+        'holiday_expect_revenu',
+    )
+    raw_id_fields = ('rest_days',)
+
+
+def _register(model, admin_class):
+    admin.site.register(model, admin_class)
+
+
+_register(models.FleetUser, FleetUserAdmin)
+_register(models.Car, CarAdmin)
+_register(models.Driver, DriverAdmin)
+_register(models.Contract, ContractAdmin)
