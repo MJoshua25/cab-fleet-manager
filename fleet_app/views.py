08aacccc8a2adab143d9f4b360d71adc20cfc5ad
paulemxx <51763335+paulemxx@users.noreply.github.com>
2023-07-23 21:59:42 +0000

    Add views driverGestion, contratGestion

--- fleet_app/views.py
+++ fleet_app/views.py
@@ -5,6 +5,20 @@ from fleet_app import models as fleet_models
 
 def carGestion(request):
     data = {
-       'cars': fleet_models.Car.objects.filter(statut=True)
+        'cars': fleet_models.Car.objects.filter(statut=True)
     }
     return render(request, 'pages/fleet/carGestion.html', data)
+
+
+def driverGestion(request):
+    data = {
+        'drivers': fleet_models.Driver.objects.filter(statut=True)
+    }
+    return render(request, 'pages/fleet/driverGestion.html', data)
+
+
+def contratGestion(request):
+    data = {
+        'contrats': fleet_models.Contract.objects.filter(statut=True)
+    }
+    return render(request, 'pages/fleet/contractGestion.html', data)
