61d40fef5f69197540f8468c401198b9250f24a5
paulemxx <51763335+paulemxx@users.noreply.github.com>
2023-07-23 21:09:11 +0000

    Add views fleet app for page carGestion

--- fleet_app/views.py
+++ fleet_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from fleet_app import models as fleet_models
+
+
+def carGestion(request):
+    data = {
+       'cars': fleet_models.Car.objects.filter(statut=True)
+    }
+    return render(request, 'pages/fleet/carGestion.html', data)
