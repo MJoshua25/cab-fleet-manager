af7710a4f6f43c28f8dd28d34686ba48ce5573a5
paulemxx <51763335+paulemxx@users.noreply.github.com>
2023-07-23 21:08:38 +0000

    Add test fleet app for page carGestion

--- fleet_app/tests.py
+++ fleet_app/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import unittest
+from django.test import Client
 
-# Create your tests here.
+
+class FleetCarTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get("/fleet/carGestion")
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
