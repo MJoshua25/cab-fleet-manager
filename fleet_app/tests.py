42304c4b7e1f3deb74e4bcfb255d0179163569b8
paulemxx <51763335+paulemxx@users.noreply.github.com>
2023-07-23 22:06:06 +0000

    Add test driverGestion, carGestion

--- fleet_app/tests.py
+++ fleet_app/tests.py
@@ -2,7 +2,7 @@ import unittest
 from django.test import Client
 
 
-class FleetCarTest(unittest.TestCase):
+class CarGestionTest(unittest.TestCase):
     def setUp(self):
         # Every test needs a client.
         self.client = Client()
@@ -10,6 +10,32 @@ class FleetCarTest(unittest.TestCase):
     def test_details(self):
         # Issue a GET request.
         response = self.client.get("/fleet/carGestion")
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+
+class DriverGestionTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get("/fleet/driverGestion")
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+
+class ContratGestionTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get("/fleet/contratGestion")
 
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
