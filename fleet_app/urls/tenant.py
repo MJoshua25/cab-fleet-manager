839fb93e42207b128a91fd73dd16e19260f0f4d5
paulemxx <51763335+paulemxx@users.noreply.github.com>
2023-08-02 21:19:04 +0000

    Add urls add car and car detail

--- fleet_app/urls/tenant.py
+++ fleet_app/urls/tenant.py
@@ -6,4 +6,7 @@ urlpatterns = [
 	path('vehicule', tenant.CarListView.as_view(), name='car_list'),
 	path('chauffeur', tenant.DriverListView.as_view(), name='driver_list'),
 	path('contrat', tenant.ContractListView.as_view(), name='contract_list'),
+	path('vehicule/ajout_vehicule', tenant.add_vehicule, name='add_tenant_vehicule'),
+	path('vehicule/detail/<int:id_art>', tenant.CarDetailView.as_view(), name='car_detail'),
+
 ]
