9840305fae7b463b1ece7a1a495e8fb20b68b0bc
paulemxx <51763335+paulemxx@users.noreply.github.com>
2023-08-07 09:51:49 +0000

    Add views form and page update

--- fleet_app/views/tenant.py
+++ fleet_app/views/tenant.py
@@ -52,6 +52,42 @@ def add_vehicule(request: HttpRequest, tenant: str) -> HttpResponse:
         return redirect("core:tenant", tenant=tenant.unique_domain)
 
 
+class CarUpdateView(TenantAwareViewMixin, DetailView):
+    model = fleet_models.Car
+    template_name = 'pages/tenant/fleet/car_modif.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
+
+
+def update_vehicule(request: HttpRequest, tenant: str, type_id: int) -> HttpResponse:
+    tenant: 'Tenant' = request.user.profile.tenant
+    if request.method == "POST":
+        model = request.POST.get('model')
+        brand = request.POST.get('brand')
+        matriculation = request.POST.get('matriculation')
+        color = request.POST.get('color')
+        on_service = request.POST.get('on_service')
+        car = fleet_models.Car.objects.filter(statut=True, id=type_id)[:1].get()
+        car.model = model,
+        car.brand = brand,
+        car.matriculation = matriculation,
+        car.color = color,
+        if on_service == 'on':
+            car.on_service = True
+        else:
+            car.on_service = False
+        print(car.on_service)
+        tenant = tenant
+        car.save()
+
+        return redirect('core:tenant:fleet:car_list', tenant=tenant.unique_domain)
+    else:
+        return redirect("core:tenant", tenant=tenant.unique_domain)
+
+
 class DriverListView(TenantAwareViewMixin, ListView):
     template_name = 'pages/tenant/fleet/driver_list.html'
 
