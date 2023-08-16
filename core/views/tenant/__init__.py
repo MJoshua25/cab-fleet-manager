8c6af4aeb40cf751636789abff0db945fef06b05
Yao Josué <46876900+MJoshua25@users.noreply.github.com>
2023-08-16 17:06:28 +0000

    taking datas from the database

--- core/views/tenant/__init__.py
+++ core/views/tenant/__init__.py
@@ -2,6 +2,20 @@ from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 from tenant.mixin import TenantAwareViewMixin
 
+from fleet_app import models as fleet_app_models
+
 
 class IndexView(TenantAwareViewMixin, TemplateView):
 	template_name = 'pages/tenant/index.html'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["cars_number"] = fleet_app_models.Car.objects.filter(
+			statut=True,
+			tenant=self.tenant
+		).count()
+		context["drivers_number"] = fleet_app_models.Driver.objects.filter(
+			statut=True,
+			tenant=self.tenant
+		).count()
+		return context
