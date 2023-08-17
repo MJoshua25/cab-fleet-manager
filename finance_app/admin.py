348eb162128f5656d8d424c35a6e78a75f73ce19
Yao Josué <46876900+MJoshua25@users.noreply.github.com>
2023-08-17 15:23:43 +0000

    create admin for finance app

--- finance_app/admin.py
+++ finance_app/admin.py
@@ -1,3 +1,64 @@
+# vim: set fileencoding=utf-8 :
 from django.contrib import admin
 
-# Register your models here.
+import finance_app.models as models
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'statut',
+        'date_add',
+        'date_upd',
+        'tenant',
+        'amount',
+        'payment_method',
+        'date_payment',
+    )
+    list_filter = (
+        'statut',
+        'date_add',
+        'date_upd',
+        'tenant',
+        'date_payment',
+        'id',
+        'amount',
+        'payment_method',
+    )
+
+
+class PaymentAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'statut',
+        'date_add',
+        'date_upd',
+        'tenant',
+        'amount',
+        'contract',
+        'payment_method',
+        'date_payment',
+        'is_sold_out',
+    )
+    list_filter = (
+        'statut',
+        'date_add',
+        'date_upd',
+        'tenant',
+        'contract',
+        'date_payment',
+        'is_sold_out',
+        'id',
+        'amount',
+        'payment_method',
+    )
+
+
+def _register(model, admin_class):
+    admin.site.register(model, admin_class)
+
+
+_register(models.Expense, ExpenseAdmin)
+_register(models.Payment, PaymentAdmin)
