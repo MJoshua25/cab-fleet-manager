8d058255b53b81ad621948340fe0c339cf9649b6
Yao Josué <46876900+MJoshua25@users.noreply.github.com>
2023-08-18 12:04:03 +0000

    update Outage model to make the driver optional

--- fleet_app/models.py
+++ fleet_app/models.py
@@ -117,7 +117,7 @@ class InsurancePayment(Expense):
 
 class Outage(Expense):
     car = models.ForeignKey(Car, related_name="outages", on_delete=models.CASCADE)
-    driver = models.ForeignKey(Driver, related_name="outages", on_delete=models.CASCADE)
+    driver = models.ForeignKey(Driver, related_name="outages", on_delete=models.CASCADE, null=True, blank=True)
     reason = models.ForeignKey(OutageReason, related_name="outages", on_delete=models.CASCADE)
     location = models.CharField(max_length=100)
     is_okay = models.BooleanField(default=False)
