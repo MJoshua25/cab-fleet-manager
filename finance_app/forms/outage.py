29db177850be0e4371f904274105ea254222b25f
Yao Josué <46876900+MJoshua25@users.noreply.github.com>
2023-08-19 06:21:41 +0000

    create OutageForm to manipulate outage data

---
+++ finance_app/forms/outage.py
@@ -0,0 +1,15 @@
+from django import forms
+from fleet_app.models import Outage
+
+
+class OutageForm(forms.ModelForm):
+	is_okay = forms.BooleanField(label='Est réglé?', initial=False, required=False)
+
+	class Meta:
+		model = Outage
+		fields = ["amount", "payment_method", "date_payment", "driver", "car", "reason", "location", "is_okay"]
+
+	def clean(self):
+		cleaned_data = super().clean()
+		if 'is_okay' not in cleaned_data:
+			cleaned_data['is_okay'] = False
