d68ee2dba4cb4f5c8d2f8bc29af300c9ba580c03
paulemxx <51763335+paulemxx@users.noreply.github.com>
2023-08-24 13:06:03 +0000

    Add test Driver, Contract List

--- fleet_app/tests.py
+++ fleet_app/tests.py
@@ -12,19 +12,43 @@ class CarListTest(unittest.TestCase):
         response = self.client.get("/fleet/vehicule")
 
         # Check that the response is 200 OK.
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
 
-class CarModifTest(unittest.TestCase):
+class CarDetailTest(unittest.TestCase):
     def setUp(self):
         # Every test needs a client.
         self.client = Client()
 
     def test_details(self):
         # Issue a GET request.
-        response = self.client.get("/fleet/vehicule/modif_vehicule")
+        response = self.client.get("/fleet/vehicule/1")
 
         # Check that the response is 200 OK.
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
 
+class DriverListTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get("/fleet/chauffeur")
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 302)
+
+
+class ContractListTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get("/fleet/contrat")
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 302)
