ef30c4006dec090c512fe3877b2feebe0a1b3b53
Yao Josué <46876900+MJoshua25@users.noreply.github.com>
2023-08-24 11:29:51 +0000

    indent is ok

--- finance_app/views/tenant.py
+++ finance_app/views/tenant.py
@@ -22,25 +22,27 @@ class InsuranceListView(TenantAwareViewMixin, ListView):
 
 	def get_queryset(self):
 		return fleet_models.Insurance.objects.filter(statut=True, tenant=self.tenant)
-  
-  def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
-      context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
-      context["week"] = core_models.DayOfTheWeek.objects.filter(tenant=self.tenant, statut=True)
-      return context
+
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
+		context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
+		context["week"] = core_models.DayOfTheWeek.objects.filter(tenant=self.tenant, statut=True)
+		return context
 
 
 class InsuranceDetailView(TenantAwareViewMixin, DetailView):
 	model = fleet_models.Insurance
 	template_name = 'pages/tenant/finance/insurance_detail.html'
 
-  def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
-      context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
-      context["week"] = core_models.DayOfTheWeek.objects.filter(tenant=self.tenant, statut=True)
-      return context
+
+def get_context_data(self, **kwargs):
+	context = super().get_context_data(**kwargs)
+	context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
+	context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
+	context["week"] = core_models.DayOfTheWeek.objects.filter(tenant=self.tenant, statut=True)
+	return context
 
 
 def add_insurance(request: HttpRequest, tenant: str) -> HttpResponse:
@@ -77,12 +79,13 @@ class InsuranceUpdateView(TenantAwareViewMixin, DetailView):
 	model = fleet_models.Insurance
 	template_name = 'pages/tenant/finance/insurance_modif.html'
 
-  def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
-      context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
-      context["week"] = core_models.DayOfTheWeek.objects.filter(tenant=self.tenant, statut=True)
-      return context
+
+def get_context_data(self, **kwargs):
+	context = super().get_context_data(**kwargs)
+	context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
+	context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
+	context["week"] = core_models.DayOfTheWeek.objects.filter(tenant=self.tenant, statut=True)
+	return context
 
 
 def update_insurance(request: HttpRequest, tenant: str, type_id: int) -> HttpResponse:
@@ -132,30 +135,32 @@ class InsurancePaymentListView(TenantAwareViewMixin, ListView):
 	def get_queryset(self):
 		return fleet_models.InsurancePayment.objects.filter(statut=True, tenant=self.tenant)
 
-  def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
-      context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
-      context["week"] = core_models.DayOfTheWeek.objects.filter(statut=True)
-      context["insurance"] = fleet_models.Insurance.objects.filter(tenant=self.tenant, statut=True)
-      context["contract"] = fleet_models.Contract.objects.filter(tenant=self.tenant, statut=True)
-      context["expense"] = finance_models.Expense.objects.filter(tenant=self.tenant, statut=True)
-      return context
+
+def get_context_data(self, **kwargs):
+	context = super().get_context_data(**kwargs)
+	context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
+	context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
+	context["week"] = core_models.DayOfTheWeek.objects.filter(statut=True)
+	context["insurance"] = fleet_models.Insurance.objects.filter(tenant=self.tenant, statut=True)
+	context["contract"] = fleet_models.Contract.objects.filter(tenant=self.tenant, statut=True)
+	context["expense"] = finance_models.Expense.objects.filter(tenant=self.tenant, statut=True)
+	return context
 
 
 class InsurancePaymentDetailView(TenantAwareViewMixin, DetailView):
 	model = fleet_models.InsurancePayment
 	template_name = 'pages/tenant/finance/insurance_payment_detail.html'
 
-  def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
-      context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
-      context["week"] = core_models.DayOfTheWeek.objects.filter(statut=True)
-      context["insurance"] = fleet_models.Insurance.objects.filter(tenant=self.tenant, statut=True)
-      context["contract"] = fleet_models.Contract.objects.filter(tenant=self.tenant, statut=True)
-      context["expense"] = finance_models.Expense.objects.filter(tenant=self.tenant, statut=True)
-      return context
+
+def get_context_data(self, **kwargs):
+	context = super().get_context_data(**kwargs)
+	context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
+	context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
+	context["week"] = core_models.DayOfTheWeek.objects.filter(statut=True)
+	context["insurance"] = fleet_models.Insurance.objects.filter(tenant=self.tenant, statut=True)
+	context["contract"] = fleet_models.Contract.objects.filter(tenant=self.tenant, statut=True)
+	context["expense"] = finance_models.Expense.objects.filter(tenant=self.tenant, statut=True)
+	return context
 
 
 def add_insurance_payment(request: HttpRequest, tenant: str) -> HttpResponse:
@@ -190,15 +195,16 @@ class InsurancePaymentUpdateView(TenantAwareViewMixin, DetailView):
 	model = fleet_models.InsurancePayment
 	template_name = 'pages/tenant/finance/insurance_payment_modif.html'
 
-  def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
-      context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
-      context["week"] = core_models.DayOfTheWeek.objects.filter(statut=True)
-      context["insurance"] = fleet_models.Insurance.objects.filter(tenant=self.tenant, statut=True)
-      context["contract"] = fleet_models.Contract.objects.filter(tenant=self.tenant, statut=True)
-      context["expense"] = finance_models.Expense.objects.filter(tenant=self.tenant, statut=True)
-      return context
+
+def get_context_data(self, **kwargs):
+	context = super().get_context_data(**kwargs)
+	context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
+	context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
+	context["week"] = core_models.DayOfTheWeek.objects.filter(statut=True)
+	context["insurance"] = fleet_models.Insurance.objects.filter(tenant=self.tenant, statut=True)
+	context["contract"] = fleet_models.Contract.objects.filter(tenant=self.tenant, statut=True)
+	context["expense"] = finance_models.Expense.objects.filter(tenant=self.tenant, statut=True)
+	return context
 
 
 def update_insurance_payment(request: HttpRequest, tenant: str, type_id: int) -> HttpResponse:
@@ -239,126 +245,126 @@ def update_insurance_payment(request: HttpRequest, tenant: str, type_id: int) ->
 
 
 def delete_insurance_payment(request: HttpRequest, tenant: str, type_id: int) -> HttpResponse:
-    tenant: 'Tenant' = request.user.profile.tenant
-    tenant = tenant
-    contract = fleet_models.InsurancePayment.objects.filter(statut=True, id=type_id)[:1].get()
-    contract.delete()
-    return redirect('core:tenant:finance:insurance_payment_list', tenant=tenant.unique_domain)
+	tenant: 'Tenant' = request.user.profile.tenant
+	tenant = tenant
+	contract = fleet_models.InsurancePayment.objects.filter(statut=True, id=type_id)[:1].get()
+	contract.delete()
+	return redirect('core:tenant:finance:insurance_payment_list', tenant=tenant.unique_domain)
 
 
 class OilChangeListView(TenantAwareViewMixin, ListView):
-    template_name = 'pages/tenant/finance/oil_change_list.html'
+	template_name = 'pages/tenant/finance/oil_change_list.html'
 
-    def get_queryset(self):
-        return fleet_models.OilChange.objects.filter(statut=True, tenant=self.tenant)
+	def get_queryset(self):
+		return fleet_models.OilChange.objects.filter(statut=True, tenant=self.tenant)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
-        context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
-        context["week"] = core_models.DayOfTheWeek.objects.filter(tenant=self.tenant, statut=True)
-        context["insurance"] = fleet_models.Insurance.objects.filter(tenant=self.tenant, statut=True)
-        context["contract"] = fleet_models.Contract.objects.filter(tenant=self.tenant, statut=True)
-        context["expense"] = finance_models.Expense.objects.filter(tenant=self.tenant, statut=True)
-        return context
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
+		context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
+		context["week"] = core_models.DayOfTheWeek.objects.filter(tenant=self.tenant, statut=True)
+		context["insurance"] = fleet_models.Insurance.objects.filter(tenant=self.tenant, statut=True)
+		context["contract"] = fleet_models.Contract.objects.filter(tenant=self.tenant, statut=True)
+		context["expense"] = finance_models.Expense.objects.filter(tenant=self.tenant, statut=True)
+		return context
 
 
 class OilChangeDetailView(TenantAwareViewMixin, DetailView):
-    model = fleet_models.OilChange
-    template_name = 'pages/tenant/finance/oil_change_detail.html'
+	model = fleet_models.OilChange
+	template_name = 'pages/tenant/finance/oil_change_detail.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
-        context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
-        context["week"] = core_models.DayOfTheWeek.objects.filter(tenant=self.tenant, statut=True)
-        context["insurance"] = fleet_models.Insurance.objects.filter(tenant=self.tenant, statut=True)
-        context["contract"] = fleet_models.Contract.objects.filter(tenant=self.tenant, statut=True)
-        context["expense"] = finance_models.Expense.objects.filter(tenant=self.tenant, statut=True)
-        return context
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
+		context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
+		context["week"] = core_models.DayOfTheWeek.objects.filter(tenant=self.tenant, statut=True)
+		context["insurance"] = fleet_models.Insurance.objects.filter(tenant=self.tenant, statut=True)
+		context["contract"] = fleet_models.Contract.objects.filter(tenant=self.tenant, statut=True)
+		context["expense"] = finance_models.Expense.objects.filter(tenant=self.tenant, statut=True)
+		return context
 
 
 def add_oil_change(request: HttpRequest, tenant: str) -> HttpResponse:
-    tenant: 'Tenant' = request.user.profile.tenant
-    if request.method == "POST":
-        amount = request.POST.get('amount')
-        payment_method = request.POST.get('payment_method')
-        date_payment = request.POST.get('date_payment')
-        oil_type = request.POST.get('oil_type')
-        car = int(request.POST.get('car'))
-        service_center = request.POST.get('service_center')
-        date_OilChange = request.POST.get('date_OilChange')
-        date_next_oil_change = request.POST.get('date_next_oil_change')
-        i = fleet_models.OilChange(
-            car_id=car,
-            amount=amount,
-            payment_method=payment_method,
-            oil_type=oil_type,
-            date_payment=datetime.datetime.strptime(date_payment, '%Y-%m-%dT%H:%M').date(),
-            service_center=service_center,
-            date_OilChange=datetime.datetime.strptime(date_OilChange, '%Y-%m-%dT%H:%M').date(),
-            date_next_oil_change=datetime.datetime.strptime(date_next_oil_change, '%Y-%m-%dT%H:%M').date(),
-            tenant=tenant
-        )
-        i.save()
-        return redirect('core:tenant:finance:oil_change_list', tenant=tenant.unique_domain)
-    else:
-        return redirect("core:tenant", tenant=tenant.unique_domain)
+	tenant: 'Tenant' = request.user.profile.tenant
+	if request.method == "POST":
+		amount = request.POST.get('amount')
+		payment_method = request.POST.get('payment_method')
+		date_payment = request.POST.get('date_payment')
+		oil_type = request.POST.get('oil_type')
+		car = int(request.POST.get('car'))
+		service_center = request.POST.get('service_center')
+		date_OilChange = request.POST.get('date_OilChange')
+		date_next_oil_change = request.POST.get('date_next_oil_change')
+		i = fleet_models.OilChange(
+			car_id=car,
+			amount=amount,
+			payment_method=payment_method,
+			oil_type=oil_type,
+			date_payment=datetime.datetime.strptime(date_payment, '%Y-%m-%dT%H:%M').date(),
+			service_center=service_center,
+			date_OilChange=datetime.datetime.strptime(date_OilChange, '%Y-%m-%dT%H:%M').date(),
+			date_next_oil_change=datetime.datetime.strptime(date_next_oil_change, '%Y-%m-%dT%H:%M').date(),
+			tenant=tenant
+		)
+		i.save()
+		return redirect('core:tenant:finance:oil_change_list', tenant=tenant.unique_domain)
+	else:
+		return redirect("core:tenant", tenant=tenant.unique_domain)
 
 
 class OilChangeUpdateView(TenantAwareViewMixin, DetailView):
-    model = fleet_models.OilChange
-    template_name = 'pages/tenant/finance/oil_change_modif.html'
+	model = fleet_models.OilChange
+	template_name = 'pages/tenant/finance/oil_change_modif.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
-        context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
-        context["week"] = core_models.DayOfTheWeek.objects.filter(tenant=self.tenant, statut=True)
-        context["insurance"] = fleet_models.Insurance.objects.filter(tenant=self.tenant, statut=True)
-        context["contract"] = fleet_models.Contract.objects.filter(tenant=self.tenant, statut=True)
-        context["expense"] = finance_models.Expense.objects.filter(tenant=self.tenant, statut=True)
-        return context
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["drivers"] = fleet_models.Driver.objects.filter(tenant=self.tenant, statut=True)
+		context["cars"] = fleet_models.Car.objects.filter(tenant=self.tenant, statut=True)
+		context["week"] = core_models.DayOfTheWeek.objects.filter(tenant=self.tenant, statut=True)
+		context["insurance"] = fleet_models.Insurance.objects.filter(tenant=self.tenant, statut=True)
+		context["contract"] = fleet_models.Contract.objects.filter(tenant=self.tenant, statut=True)
+		context["expense"] = finance_models.Expense.objects.filter(tenant=self.tenant, statut=True)
+		return context
 
 
 def update_oil_change(request: HttpRequest, tenant: str, type_id: int) -> HttpResponse:
-    u_tenant: 'Tenant' = request.user.profile.tenant
-    tenant = u_tenant
+	u_tenant: 'Tenant' = request.user.profile.tenant
+	tenant = u_tenant
 
-    if request.method != "POST":
-        return redirect("core:tenant", tenant=tenant.unique_domain)
+	if request.method != "POST":
+		return redirect("core:tenant", tenant=tenant.unique_domain)
 
-    # Getting datas from the form
-    amount = request.POST.get('amount')
-    payment_method = request.POST.get('payment_method')
-    date_payment = request.POST.get('date_payment')
-    car = int(request.POST.get('car'))
-    oil_type=request.POST.get('oil_type')
-    service_center = request.POST.get('service_center')
-    date_OilChange = request.POST.get('date_OilChange')
-    date_next_oil_change = request.POST.get('date_next_oil_change')
+	# Getting datas from the form
+	amount = request.POST.get('amount')
+	payment_method = request.POST.get('payment_method')
+	date_payment = request.POST.get('date_payment')
+	car = int(request.POST.get('car'))
+	oil_type = request.POST.get('oil_type')
+	service_center = request.POST.get('service_center')
+	date_OilChange = request.POST.get('date_OilChange')
+	date_next_oil_change = request.POST.get('date_next_oil_change')
 
-    # Updating the corresponding car object
-    oilchange = fleet_models.OilChange.objects.filter(statut=True, id=type_id)[:1].get()
-    oilchange.amount = amount
-    oilchange.payment_method = payment_method
-    oilchange.date_payment = datetime.datetime.strptime(date_payment, '%Y-%m-%dT%H:%M').date()
-    oilchange.date_payment = str(oilchange.date_payment)
-    oilchange.car_id = car
-    oilchange.service_center = service_center
-    oilchange.oil_type = oil_type
-    oilchange.date_OilChange = datetime.datetime.strptime(date_OilChange, '%Y-%m-%dT%H:%M').date()
-    oilchange.date_OilChange = str(oilchange.date_OilChange)
-    oilchange.date_next_oil_change = datetime.datetime.strptime(date_next_oil_change, '%Y-%m-%dT%H:%M').date()
-    oilchange.date_next_oil_change = str(oilchange.date_next_oil_change)
+	# Updating the corresponding car object
+	oilchange = fleet_models.OilChange.objects.filter(statut=True, id=type_id)[:1].get()
+	oilchange.amount = amount
+	oilchange.payment_method = payment_method
+	oilchange.date_payment = datetime.datetime.strptime(date_payment, '%Y-%m-%dT%H:%M').date()
+	oilchange.date_payment = str(oilchange.date_payment)
+	oilchange.car_id = car
+	oilchange.service_center = service_center
+	oilchange.oil_type = oil_type
+	oilchange.date_OilChange = datetime.datetime.strptime(date_OilChange, '%Y-%m-%dT%H:%M').date()
+	oilchange.date_OilChange = str(oilchange.date_OilChange)
+	oilchange.date_next_oil_change = datetime.datetime.strptime(date_next_oil_change, '%Y-%m-%dT%H:%M').date()
+	oilchange.date_next_oil_change = str(oilchange.date_next_oil_change)
 
-    oilchange.save()
-    return redirect('core:tenant:finance:oil_change_list', tenant=tenant.unique_domain)
+	oilchange.save()
+	return redirect('core:tenant:finance:oil_change_list', tenant=tenant.unique_domain)
 
 
 def delete_oil_change(request: HttpRequest, tenant: str, type_id: int) -> HttpResponse:
-    tenant: 'Tenant' = request.user.profile.tenant
-    tenant = tenant
-    oilchange = fleet_models.OilChange.objects.filter(statut=True, id=type_id)[:1].get()
-    oilchange.delete()
-    return redirect('core:tenant:finance:oil_change_list', tenant=tenant.unique_domain)
+	tenant: 'Tenant' = request.user.profile.tenant
+	tenant = tenant
+	oilchange = fleet_models.OilChange.objects.filter(statut=True, id=type_id)[:1].get()
+	oilchange.delete()
+	return redirect('core:tenant:finance:oil_change_list', tenant=tenant.unique_domain)
