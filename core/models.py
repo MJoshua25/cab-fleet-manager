43f4c53e3b483778b485bb2b4e73251e2a0a081f
paulemxx <51763335+paulemxx@users.noreply.github.com>
2024-03-14 12:39:28 +0000

    Création du modèle Contact dans core

--- core/models.py
+++ core/models.py
@@ -4,24 +4,37 @@ from .fields import DayOfTheWeekField, DAY_OF_THE_WEEK
 
 # Create your models here.
 class StandardModel(models.Model):
-	statut = models.BooleanField(default=True)
-	date_add = models.DateTimeField(auto_now_add=True)
-	date_upd = models.DateTimeField(auto_now=True)
+    statut = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_upd = models.DateTimeField(auto_now=True)
 
-	class Meta:
-		abstract = True
+    class Meta:
+        abstract = True
 
 
 class DayOfTheWeek(models.Model):
-	day = DayOfTheWeekField(unique=True)
+    day = DayOfTheWeekField(unique=True)
 
-	class Meta:
-		verbose_name = "DayOfTheWeek"
-		verbose_name_plural = "DayOfTheWeeks"
+    class Meta:
+        verbose_name = "DayOfTheWeek"
+        verbose_name_plural = "DayOfTheWeeks"
 
-	def __str__(self):
-		return str(self.day)
+    def __str__(self):
+        return str(self.day)
 
-	@property
-	def in_str(self):
-		return DAY_OF_THE_WEEK[self.day]
+    @property
+    def in_str(self):
+        return DAY_OF_THE_WEEK[self.day]
+
+
+class Contact(StandardModel):
+    full_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=255)
+    message = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Contact"
+        verbose_name_plural = "Contacts"
+
+    def __str__(self):
+        return f"{self.full_name} {self.message} "
